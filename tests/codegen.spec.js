42425a74bc32138d9cbc430c7cb457bad596a765
khurram-syed <khurram245@yahoo.com>
2025-08-25 17:37:07 +0100

    Updated condegen.spec.js file

--- tests/codegen.spec.js
+++ tests/codegen.spec.js
@@ -1,5 +1,5 @@
 import {test, expect} from '@playwright/test';
-
+// npx playwright codegen demo.playwright.dev/todomvc -- to record the steps quickly
 test('Testing the CodeGen tool for TodoMVC', async ({ page }) => {
   await page.goto('https://demo.playwright.dev/todomvc/#/');
   await expect(page.getByRole('textbox', { name: 'What needs to be done?' })).toBeVisible();
@@ -7,6 +7,7 @@ test('Testing the CodeGen tool for TodoMVC', async ({ page }) => {
   await page.getByRole('textbox', { name: 'What needs to be done?' }).click();
   await expect(page.getByRole('textbox', { name: 'What needs to be done?' })).toBeVisible();
   await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('this is to do list');
-  await expect(page.getByRole('textbox', { name: 'What needs to be done?' })).toHaveValue('this is to do list');});
+  await expect(page.getByRole('textbox', { name: 'What needs to be done?' })).toHaveValue('this is to do list');
+});
   
    
