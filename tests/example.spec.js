12a4171fc07c01fc50d5839df149fdb550a7292c
khurram-syed <khurram245@yahoo.com>
2025-08-25 17:45:45 +0100

    Added a failed test in example.spec.js to see failed results too

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -17,3 +17,13 @@ test('Page get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('Page get started link- Failing TESTß', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Updatingß' })).toBeVisible();
+});
